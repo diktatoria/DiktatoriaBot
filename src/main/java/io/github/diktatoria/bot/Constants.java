b45be521cfa9eea93b4463f729bcb0013ed7e164
DrDeee <63226814+DrDeee@users.noreply.github.com>
2020-11-03 19:06:40 +0100

    >accept suport

--- src/main/java/io/github/diktatoria/bot/Constants.java
+++ src/main/java/io/github/diktatoria/bot/Constants.java
@@ -1,8 +1,20 @@
 package io.github.diktatoria.bot;
 
+import com.vdurmont.emoji.EmojiParser;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+import java.awt.*;
+
 public class Constants {
     public static final long ACCEPTED_RULES = 773176626484609067L;
     public static final long ORGA = 762646703978577942L;
 
     public static final long SERVER = 762644745511239690L;
+
+    public static final EmbedBuilder ERROR_EMBED = new EmbedBuilder()
+            .setFooter("Mit " + EmojiParser.parseToUnicode(":heart:") + " entwickelt.")
+            .setColor(Color.RED);
+    public static final EmbedBuilder SUCESS_EMBED = new EmbedBuilder()
+            .setFooter("Mit " + EmojiParser.parseToUnicode(":heart:") + " entwickelt.")
+            .setColor(Color.GREEN);
 }
--- src/main/java/io/github/diktatoria/bot/listeners/RuleAcceptListener.java
+++ src/main/java/io/github/diktatoria/bot/listeners/RuleAcceptListener.java
@@ -1,21 +1,30 @@
 package io.github.diktatoria.bot.listeners;
 
 import io.github.diktatoria.bot.Constants;
-import org.javacord.api.entity.permission.Role;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RuleAcceptListener implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
         if (event.getMessage().getContent().equals(">accept")) {
             event.getMessageAuthor().asUser().ifPresent(user -> {
-                for(Role r : user.getRoles(event.getApi().getServerById(Constants.SERVER).get())){
-                    if(r.getId() != Constants.ACCEPTED_RULES){
-                        user.addRole(event.getApi().getRoleById(Constants.ACCEPTED_RULES).get());
-                        break;
-                    }
-
+                AtomicBoolean b = new AtomicBoolean(false);
+                user.getRoles(event.getServer().get()).forEach(role -> {
+                    if(role.getId() == Constants.ACCEPTED_RULES)
+                        b.set(true);
+                });
+                if(!b.get()){
+                    user.addRole(event.getApi().getRoleById(Constants.ACCEPTED_RULES).get(), "Hat die Regeln aktzeptiert");
+                    event.getChannel().sendMessage(Constants.SUCESS_EMBED
+                    .setTitle("Regeln erfolgreich aktzeptiert!")
+                    .setDescription(user.getMentionTag() + ", du bist nun ein vollwertiges Mitglied von Diktatoria und kannst dir nun in <#769228580357668874> Rollen zuweisen."));
+                }else {
+                    event.getChannel().sendMessage(Constants.ERROR_EMBED
+                    .setTitle("Fehler")
+                    .setDescription("Du hast die Regeln bereits aktzeptiert!"));
                 }
             });
 
